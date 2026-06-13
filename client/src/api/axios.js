2f64a2cecae812da37a48a49dc32d210cb0f349f
SUDHANSHU SEKHER MOHARANA <156045025+MoharanaSudhanshu@users.noreply.github.com>
2026-06-13 22:42:11 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://driveease-api-7tgx.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
